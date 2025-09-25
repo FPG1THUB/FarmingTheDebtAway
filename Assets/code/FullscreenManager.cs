bff8e144d0f1269aabf58cff9b513a050b2532a6
GrubbyDucky <elijahmcgowan008@Outlook.com>
2025-09-26 05:51:58 +0800

    UI Update
    added Fullscreen Manager
    connected UI with managers

--- Assets/code/FullscreenManager.cs
+++ Assets/code/FullscreenManager.cs
@@ -1,16 +1,56 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FullscreenManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private int _fullscreen = 0;
+    [Header("Toggle References")]
+    [SerializeField] Toggle _exclusiveFullscreenToggle;
+    [SerializeField] Toggle _fullscreenWindowToggle;
+    [SerializeField] Toggle _windowedToggle;
+
+    public int CurrentFullscreenMode
     {
-        
+        set { _fullscreen = value; }
+        get { return _fullscreen; }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        switch (CurrentFullscreenMode)
+        {
+            case 0:
+                _exclusiveFullscreenToggle.isOn = true;
+                break;
+            case 1:
+                _fullscreenWindowToggle.isOn = true;
+                break;
+            case 2:
+                _windowedToggle.isOn = true;
+                break;
+            default:
+                _exclusiveFullscreenToggle.isOn = true;
+                break;
+        }
+    }
+
+    public void SetScreenMode(int mode)
+    {
+        CurrentFullscreenMode = mode;
+        switch (CurrentFullscreenMode)
+        {
+            case 0:
+                Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                break;
+            case 1:
+                Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+                break;
+            case 2:
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+                break;
+            default:
+                Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                break;
+        }
     }
 }
