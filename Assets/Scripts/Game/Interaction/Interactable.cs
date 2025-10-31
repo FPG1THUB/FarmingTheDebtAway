7f4f25f1fc2dd4544fdb323c9c4ac6f46d241b84
EthanF11 <ethan.flemming11@gmail.com>
2025-10-31 16:03:53 +0800

    Revert "Feature/time (#6)"
    This reverts commit 7c6cae6c34b691240c153ca4c643c0ecb7c7ee94.

--- Assets/Scripts/Game/Interaction/Interactable.cs
+++
@@ -1,9 +0,0 @@
-//public interface Interactable: This defines an interface called Interactable.
-public interface Interactable // Attach to script to make it Interactable.
-{
-    // In the script being added, right click Interactable, and click "Quick Actions and Refactoring" then implementing
-
-    //void OnInteraction(): This is a method that any class implementing Interactable must define.
-    void OnInteraction(); // if need an example, look at TestInteract to see it.
-    string ToolTip();
-}
