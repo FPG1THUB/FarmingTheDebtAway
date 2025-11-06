cf6faa3ab446c61a368aa33650b7f2576deb2690
Cuber3000 <videogamerz360@hotmail.com>
2025-11-06 14:44:52 +0800

    mod Inventory Interaction Move and Camera
    Included the scripts for movement, inventory, camera and interaction as well as prefabs for easier setups.

---
+++ Assets/Scripts/Game/Inventory/Inventory.cs
@@ -0,0 +1,144 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject[] hotbarSlots = new GameObject[8];
+    public GameObject[] hotbarNames = new GameObject[8];
+    public GameObject[] hotbarAmount = new GameObject[8];
+    public List<Item> inventory = new List<Item>();
+    public int _selectedHotbarIndex = 0;
+    public int money;
+
+
+    public void Start()
+    {
+        // connect our hotbar slots to the UI elements
+        ConnectHotBar();
+
+        UpdateHotBarDisplay();
+    }
+
+    void ConnectHotBar()
+    {
+        for (int i = 0; i < hotbarSlots.Length; i++)
+        {
+            hotbarSlots[i] = GameObject.Find("Slot_"+i+"_Image");
+        }
+        for (int i = 0; i < hotbarNames.Length; i++)
+        {
+            hotbarNames[i] = GameObject.Find("Slot_" + i+"_Text");
+        }
+        for (int i = 0; i < hotbarAmount.Length; i++)
+        {
+            hotbarAmount[i] = GameObject.Find("Slot_" + i + "_Amount");
+        }
+    }
+    public void UpdateHotBarDisplay()
+    {
+        for (int i = 0; i < inventory.Count; i++)
+        {
+
+            hotbarSlots[i].GetComponent<Image>().sprite = inventory[i].ItemIcon;
+            hotbarNames[i].GetComponent<Text>().text = inventory[i].ItemName;
+            hotbarAmount[i].GetComponent<Text>().text = "x " + inventory[i].ItemQuantity;
+            if (inventory[i].ItemQuantity <= 0)
+            {
+                inventory.Remove(inventory[i]); // Clears out the inventory slot.
+            }
+        }
+    }
+    private void Update()
+    {
+        SelectingHotbarSlot();
+       // Debug.Log(inventory[_selectedHotbarIndex]);
+    }
+
+    private void SelectingHotbarSlot()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            _selectedHotbarIndex = 0;
+            Debug.Log(_selectedHotbarIndex);
+
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            _selectedHotbarIndex = 1;
+            Debug.Log(_selectedHotbarIndex);
+
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            _selectedHotbarIndex = 2;
+            Debug.Log(_selectedHotbarIndex);
+
+        }
+        else if (Input.GetKeyDown("4"))
+        {
+            _selectedHotbarIndex = 3;
+            Debug.Log(_selectedHotbarIndex);
+
+        }
+        else if (Input.GetKeyDown("5"))
+        {
+            _selectedHotbarIndex = 4;
+            Debug.Log(_selectedHotbarIndex);
+
+        }
+        else if (Input.GetKeyDown("6"))
+        {
+            _selectedHotbarIndex = 5;
+            Debug.Log(_selectedHotbarIndex);
+
+        }
+        else if (Input.GetKeyDown("7"))
+        {
+            _selectedHotbarIndex = 6;
+            Debug.Log(_selectedHotbarIndex);
+
+        }
+        else if (Input.GetKeyDown("8"))
+        {
+            _selectedHotbarIndex = 7;
+            Debug.Log(_selectedHotbarIndex);
+
+        }
+    }
+    #region My attempt
+    /*
+        [Serializable]
+        public struct InventoryData // Holds the information for each of the inventory slotws.
+        {
+            public string inventoryName; // Name of the inventory Slot, going from 0-5 atm.
+            public Image inventorySprite; // The image within the inventory slot
+            public string inventoryID; // The item currently being stored there.
+
+        }
+        [Header("Inventory")]
+        [SerializeField] InventoryData[] _inventoryData; // creates a viewable array in te inspector to mess around with.
+
+        [SerializeField] GameObject _selectedSlot; // will use this for the selected slot and image in total.
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }*/
+    #endregion
+
+    void UseItem()
+    {
+        //if ()
+        //{
+
+        //}
+    }
+} 
---
+++ Assets/Scripts/Game/Inventory/Item.cs
@@ -0,0 +1,68 @@
+using System.ComponentModel;
+using UnityEngine;
+
+public class Item
+{
+    // private variables of all item stats
+    private int _itemId;
+    private string _itemName;
+    private string _itemDescription;
+    private int _itemValue;
+    private Sprite _itemIcon;
+    private string _itemMesh;
+    private ItemType _itemType;
+    private int _itemQuantity;
+    // public properties to access stats
+
+    // Below are the series of functions to over ride the known item details with details from the itemData script.
+    public int ItemId
+    {
+        get { return _itemId; }
+        set { _itemId = value; }
+    }
+    public string ItemName
+    {
+        get { return _itemName; }
+        set { _itemName = value; }
+
+    }
+    public string ItemDescription
+    {
+        get { return _itemDescription; }
+        set { _itemDescription = value; }
+    }
+
+    public int Value
+    {
+        get { return _itemValue; }
+        set { _itemValue = value; }
+    }
+    public Sprite ItemIcon
+    {
+        get { return _itemIcon; }
+        set { _itemIcon = value; }
+    }
+
+    public string Mesh
+    {
+        get { return _itemMesh; }
+        set { _itemMesh = value; }
+    }
+    public ItemType Type
+    {
+        get { return _itemType; }
+        set { _itemType = value; }
+    }
+    public int ItemQuantity
+    {
+        get { return _itemQuantity; }
+        set { _itemQuantity = value; }
+    }
+   
+}
+public enum ItemType // Can add more types as necessary.
+{
+    Seed,
+    Crop,
+    Tool 
+}
